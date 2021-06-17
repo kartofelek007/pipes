cb48c6ba45061ea55a4fd408c5807a6812941803
karto <kartofelek007@gmail.com>
2021-06-17 19:54:44 +0200

    praca

--- js/app.js
+++ js/app.js
@@ -51,12 +51,14 @@ const typesWithPointBottom = tileTypes.filter(tile => tile.points && tile.points
 const typesMustActive = tileTypes.filter(tile => [16,17,18,19,20,21,22,23].includes(tile.type)).map(tile => tile.type);
 const cnt = document.querySelector('.canvas');
 const movesEl = document.querySelector(".moves");
+const dragPipesCnt = document.querySelector('.parts-cnt');
 
 class Level {
     constructor(levelNr) {
         this.moves = 0;
         this.startTime = new Date();
-        this.levelPattern = levels[levelNr].flat(Infinity);
+        this.levelPattern = levels[levelNr].pattern.flat(Infinity);
+        this.missedPart = levels[levelNr]?.missed;
         this.rowCount = 0; //liczba elementow w rzędzie
         this.colCount = 0; //liczba elementow w kolumnie
         this.level = this.parseLevelText();
@@ -64,6 +66,26 @@ class Level {
         this.endPoints = []; //ale koncówek może być kilka
 
         this.clickOnTile = this.clickOnTile.bind(this);
+        this.generateMissedPart();
+    }
+
+    generateMissedPart() {
+        dragPipesCnt.innerHTML = "";
+
+        if (this.missedPart) {
+            [...this.missedPart].forEach((char, i) => {
+                const ob = tileTypes.find(ob => ob.icon === char);
+                const div = this.generatePipePart(ob);
+                div.draggable = true;
+                div.dataset.static = true;
+                div.id = "pipe_" + i;
+                const divCnt = document.createElement("div");
+                divCnt.classList.add("parts-pipe-place");
+                divCnt.append(div);
+                dragPipesCnt.append(divCnt);
+            })
+
+        }
     }
 
     parseLevelText() {
@@ -83,11 +105,13 @@ class Level {
     }
 
     drawActiveTiles() {
-        const divs = document.querySelectorAll(".canvas .pipe");
+        const divs = document.querySelectorAll(".canvas .pipe-cnt");
         for (let y = 0; y < this.level.length; y++) {
             for (let x = 0; x < this.level[y].length; x++) {
-                divs[y * this.colCount + x].classList.remove("pipe-active");
-                if (this.level[y][x].active) divs[y * this.colCount + x].classList.add("pipe-active");
+                if (divs[y * this.colCount + x].firstElementChild) {
+                    divs[y * this.colCount + x].classList.remove("pipe-active");
+                    if (this.level[y][x].active) divs[y * this.colCount + x].classList.add("pipe-active");
+                }
             }
         }
     }
@@ -107,7 +131,9 @@ class Level {
         }
 
         if (tilesActive >= tilesToActive) {
-            cnt.querySelectorAll(".canvas .pipe").forEach(el => el.removeEventListener("click", this.clickOnTile));
+            //cnt.querySelectorAll(".canvas .pipe").forEach(el => el.removeEventListener("click", this.clickOnTile));
+            cnt.removeEventListener("click", this.clickOnTile);
+
             const endTime = new Date().getTime();
             const time = endTime - this.startTime;
             const timeText = new Date(time * 1000).toISOString().substr(11, 8)
@@ -207,28 +233,54 @@ class Level {
     }
 
     clickOnTile(e) {
-        const div = e.currentTarget;
-        const rotate = parseInt(getComputedStyle(div).getPropertyValue("--rotate"));
-        div.style.setProperty("--rotate", `${rotate + 90}deg`)
+        const div = e.target.closest(".pipe");
+        if (div && !div.dataset.static) {
+            const rotate = parseInt(getComputedStyle(div).getPropertyValue("--rotate"));
+            div.style.setProperty("--rotate", `${rotate + 90}deg`)
 
-        this.moves++;
-        const strMoves = String(this.moves);
-        const strLen = Math.max(2, strMoves.length);
-        movesEl.innerHTML = strMoves.padStart(strLen, "0");
+            this.moves++;
+            const strMoves = String(this.moves);
+            const strLen = Math.max(2, strMoves.length);
+            movesEl.innerHTML = strMoves.padStart(strLen, "0");
 
-        this.changePipe(div.dataset.x, div.dataset.y, div);
-        this.resetTileStatus();
-        this.checkPipeConnection(this.startPoint.x, this.startPoint.y);
+            this.changePipe(div.parentElement.dataset.x, div.parentElement.dataset.y, div);
+            this.resetTileStatus();
+            this.checkPipeConnection(this.startPoint.x, this.startPoint.y);
 
-        div.addEventListener("transitionend", e => {
-            this.drawActiveTiles();
-            this.checkEndLevel();
-        }, {once: true})
+            div.addEventListener("transitionend", e => {
+                this.drawActiveTiles();
+                this.checkEndLevel();
+            }, {once: true})
+        }
+    }
+
+    generatePipePart(ob) {
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <div class="pipe-inside">
+                <div class="pipe-active-textures">
+                    <div class="pipe-active-texture1"></div>
+                    <div class="pipe-active-texture2"></div>
+                    <div class="pipe-active-texture3"></div>
+                </div>
+            </div>
+        `;
+        div.classList.add("pipe");
+        if (ob.active) {
+            div.classList.add("pipe-active");
+        }
+        if (ob.static) {
+            div.dataset.static = true;
+        }
+        div.dataset.type = ob.type;
+        div.active = true;
+        return div;
     }
 
     createCanvas() {
         this.rowCount = this.level.length;
         this.colCount = this.level[0].length;
+
         cnt.innerHTML = "";
         cnt.style.gridTemplateColumns = `repeat(${this.colCount}, 1fr)`;
         cnt.style.gridTemplateRows = `repeat(${this.rowCount}, 1fr)`;
@@ -236,33 +288,24 @@ class Level {
         for (let y = 0; y < this.level.length; y++) {
             for (let x = 0; x < this.level[y].length; x++) {
                 const ob = this.level[y][x];
-                const div = document.createElement("div");
-                div.innerHTML = `
-                    <div class="pipe-inside">
-                        <div class="pipe-active-textures">
-                            <div class="pipe-active-texture1"></div>
-                            <div class="pipe-active-texture2"></div>
-                            <div class="pipe-active-texture3"></div>
-                        </div>
-                    </div>
-                `;
-                div.classList.add("pipe");
-                if (ob.active) {
-                    div.classList.add("pipe-active");
+
+                const divCnt = document.createElement("div");
+                divCnt.classList.add("pipe-cnt");
+
+                if (ob.type !== 0) {
+                    const div = this.generatePipePart(ob);
+                    divCnt.append(div);
+                } else {
+                    divCnt.classList.add("pipe-cnt-empty");
                 }
-                if (ob.static) {
-                    div.dataset.static = true;
-                }
-                div.dataset.type = ob.type;
-                div.dataset.x = x;
-                div.dataset.y = y;
-                div.active = true;
-                if (!ob.static) {
-                    div.addEventListener("click", this.clickOnTile);
-                }
-                cnt.append(div);
+
+                divCnt.dataset.x = x;
+                divCnt.dataset.y = y;
+                cnt.append(divCnt);
             }
         }
+
+        cnt.addEventListener("click", this.clickOnTile)
     }
 
     init() {
@@ -285,11 +328,12 @@ class Level {
             this.createCanvas();
             this.checkPipeConnection(this.startPoint.x, this.startPoint.y);
             this.drawActiveTiles();
+            dragInit();
         }
     }
 }
 
-let lv = 0;
+let lv = 4;
 const level = new Level(lv);
 level.init();
 
@@ -306,28 +350,132 @@ btn.onclick = e => {
 
 
 
+function dragInit() {
+    const dragPipes = document.querySelectorAll('.parts-cnt .pipe');
+    const dropPlaces = document.querySelectorAll('.pipe-cnt-empty');
+    const trash = document.querySelector('.trash');
+    console.log(dragPipes);
+    console.log(dropPlaces);
 
+// drop areas
+    for (const drag of dragPipes) {
+        drag.addEventListener('dragstart', dragStart);
+        drag.addEventListener('dragend', dragEnd);
+    }
 
+// Loop through empty boxes and add listeners
+    for (const place of dropPlaces) {
+        console.log(place);
+        document.addEventListener('dragover', dragOver);
+        document.addEventListener('dragenter', dragEnter);
+        document.addEventListener('dragleave', dragLeave);
+        document.addEventListener('drop', dragDrop);
+    }
 
+// Drag Functions
 
+    let from, to;
 
+    function dragStart(e) {
+        e.dataTransfer.setData("text", e.target.id);
+        if (e.target.parentElement.classList.contains("parts-pipe-place")) {
+            e.target.fromDiv = e.target.parentElement;
+            console.log(e.target.fromDiv);
+        }
+        e.effectAllowed = "move";
+        from = e.currentTarget.closest(".pipe-cnt");
+        this.classList.add('dragged');
+        setTimeout(() => {
+            this.classList.add('invisible');
+        }, 0);
+    }
 
+    function dragEnd() {
+        this.classList.remove('invisible');
+        this.classList.remove('dragged');
+    }
 
+    function dragOver(e) {
+        e.preventDefault();
+    }
 
+    function dragEnter(e) {
+        e.preventDefault();
+        if (!e.target.closest(".pipe-cnt:empty")) return;
+        const el = e.target.closest(".pipe-cnt:empty");
+        el.classList.add("hovered");
+        el.classList.add("pipe-cnt-dragged-placed");
+    }
 
+    function dragLeave(e) {
+        if (!e.target.closest(".pipe-cnt:empty")) return;
+        const el = e.target.closest(".pipe-cnt:empty");
+        el.classList.remove("hovered");
+        el.classList.remove("pipe-cnt-dragged-placed");
+    }
 
+    function dragDrop(e) {
+        if (!e.target.closest(".pipe-cnt:empty")) return;
+        const el = e.target.closest(".pipe-cnt:empty");
 
+        el.classList.remove("hovered");
+        el.classList.remove("invisible", "dragged");
 
+        const id = e.dataTransfer.getData("text");
 
+        if (from) {
+            const typeZero = tileTypes.find(tile => tile.type === 0);
+            level.level[from.dataset.y][from.dataset.x] = {...typeZero};
+            from.classList.remove("pipe-cnt-dragged-placed");
+        }
 
+        if (from !== to) {
+            level.moves++;
+            const strMoves = String(level.moves);
+            const strLen = Math.max(2, strMoves.length);
+            movesEl.innerHTML = strMoves.padStart(strLen, "0");
+        }
 
+        const draggedOriginal = document.querySelector("#" + id);
+        draggedOriginal.classList.remove("invisible", "dragged")
+        el.append(draggedOriginal);
 
+        const pipeType = tileTypes.find(tile => tile.type === +draggedOriginal.dataset.type);
+        level.level[el.dataset.y][el.dataset.x] = {...pipeType};
 
+        level.resetTileStatus();
+        level.checkPipeConnection(level.startPoint.x, level.startPoint.y);
+        level.drawActiveTiles();
+        level.checkEndLevel();
+    }
 
+    document.addEventListener('dragover', dragOverTrash);
+    document.addEventListener('dragenter', dragEnterTrash);
+    document.addEventListener('dragleave', dragLeaveTrash);
+    document.addEventListener('drop', dragDropTrash);
 
+    function dragOverTrash(e) {
+    }
 
+    function dragEnterTrash(e) {
+    }
 
+    function dragLeaveTrash(e) {
+    }
 
+    function dragDropTrash(e) {
+        if (e.target === trash && from) {
+            const id = e.dataTransfer.getData("text");
+            const typeZero = tileTypes.find(tile => tile.type === 0);
+            level.level[from.dataset.y][from.dataset.x] = {...typeZero};
 
+            const draggedOriginal = document.querySelector("#" + id);
+            draggedOriginal.fromDiv.append(draggedOriginal);
 
-
+            level.moves++;
+            const strMoves = String(level.moves);
+            const strLen = Math.max(2, strMoves.length);
+            movesEl.innerHTML = strMoves.padStart(strLen, "0");
+        }
+    }
+}
