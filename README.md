82c76d32cbe3020c828c02ebdf98e7d53e110204
karto <kartofelek007@gmail.com>
2021-06-20 01:13:29 +0200

    skrypt deploy.js + usuniecie dist

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Eksperyment w postaci gry w układanie rurek.
 
 ![](./_readme-data/screenshoot.png)
 
-Demo: 
+Demo: https://kartofelek007.github.io/pipes/
--- js/levels.js
+++ js/levels.js
@@ -50,7 +50,7 @@ export default [
             ["●╚│┌●"],
             ["●┌┌┌◄"],
         ],
-        missed: "─┤"
+        missed: "─"
     },
     {
         pattern: [
